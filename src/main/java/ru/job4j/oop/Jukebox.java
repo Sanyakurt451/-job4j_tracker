1262db84d3dbae127b4d4c458edf5e9832cd7729
sanya <batalovalexande@gmail.com>
2022-08-06 21:31:40 +0500

    1.3. Вызов метода с аргументами. [#166086]

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+
+    public void music(int position) {
+        if(position == 1) {
+            System.out.println("Пусть бегут неуклюжие");
+        } else if( position == 2) {
+            System.out.println("Спокойной ночи");
+        } else {
+            System.out.println("Песня не найдена");
+        }
+
+    }
+    public static void main(String[] args) {
+        Jukebox vasya = new Jukebox();
+        vasya.music(1);
+        vasya.music(2);
+        vasya.music(3);
+    }
+}
