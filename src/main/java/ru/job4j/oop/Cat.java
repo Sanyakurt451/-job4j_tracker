4c054d4fd78c4d2b98c1b5b654a122ffdee0c629
sanya <batalovalexande@gmail.com>
2022-08-09 18:09:34 +0500

    1.5. Поля объекта [#166083 #329658]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -4,6 +4,8 @@ public class Cat {
 
     private String food;
 
+    private String name;
+
     public void show() {
         System.out.println(this.food);
         System.out.println(this.name);
@@ -12,8 +14,6 @@ public class Cat {
     public void eat(String meat) {
         this.food = meat;
     }
-
-    private String name;
 
     public void giveNick(String nick) {
         this.name = nick;
