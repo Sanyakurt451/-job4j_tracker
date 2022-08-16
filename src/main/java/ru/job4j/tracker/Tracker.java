2ced86ecdee07c281dda754cdefcf9e3939deab4
sanya <batalovalexande@gmail.com>
2022-08-16 21:43:20 +0500

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -5,6 +5,34 @@ public class Tracker {
     private int ids = 1;
     private int size = 0;
 
+    private int id;
+
+    private String name;
+
+    public Tracker(String name) {
+        this.name = name;
+    }
+
+    public Tracker(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Tracker() {
+    }
+
+    public void printInfo() {
+        System.out.println(name);
+        System.out.println(id);
+    }
+
+    public static void main(String[] args) {
+        Tracker first = new Tracker();
+        first.printInfo();
+        Tracker second = new Tracker("Privet", 20);
+        second.printInfo();
+    }
+
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
