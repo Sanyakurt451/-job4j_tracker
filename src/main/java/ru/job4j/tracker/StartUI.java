a3d6971440140160fe126c93e6d530ac7b8d820c
sanya <batalovalexande@gmail.com>
2022-08-18 15:37:36 +0500

    7. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        item.getCreated();
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        System.out.println("Текущие дата и время до форматирования: " + currentDateTime);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = currentDateTime.format(formatter);
+        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+    }
+}
