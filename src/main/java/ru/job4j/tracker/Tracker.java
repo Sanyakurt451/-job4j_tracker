08ae32d8f95dd570150a422610827e66d4659791
sanya <batalovalexande@gmail.com>
2022-08-22 20:43:21 +0500

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,27 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] item = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+         if (item[i] != null) {
+             item[size] = items[i];
+             size++;
+           }
+        }
+        return Arrays.copyOf(item, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] it = new Item[size];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            if (items[index].getName().equals(key)) {
+                size++;
+            }
+        }
+        return Arrays.copyOf(it, size);
     }
 }
