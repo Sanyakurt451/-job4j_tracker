c2eee0f3f6666800e40e2ed63e80db30ea17c793
sanya <batalovalexande@gmail.com>
2022-09-01 19:59:02 +0500

    3. Интерфейс Input [#181092 #337260]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,16 +4,16 @@ public class Bus implements Transport {
 
     @Override
     public void drive() {
-
+        System.out.println("Автобус едет");
     }
 
     @Override
     public void passengers(int number) {
-
+        System.out.println(20);
     }
 
     @Override
     public double fuel(double amount) {
-        return amount;
+        return 30 * amount;
     }
 }
