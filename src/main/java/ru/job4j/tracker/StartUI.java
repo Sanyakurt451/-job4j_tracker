c6f5830457960c2694fdde865ebad287830b546c
sanya <batalovalexande@gmail.com>
2022-09-01 20:09:41 +0500

    4.1. Разрыв зависимости StartUI от Scanner. [#181778 #338267]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,8 +5,7 @@ public class StartUI {
         boolean run = true;
         while (run) {
             showMenu();
-            System.out.print("Select: ");
-            int select = Integer.parseInt(input.askStr("Enter your ID"));
+            int select = input.askInt("Select: ");
             if (select == 0) {
                 System.out.println("=== Create a new Item ===");
                 System.out.print("Enter name: ");
@@ -26,7 +25,7 @@ public class StartUI {
                 }
             } else if (select == 2) {
                 System.out.println("=== Edit item ===");
-                int id = Integer.parseInt(input.askStr("Enter your ID"));
+                int id = input.askInt("Enter your ID");
                 System.out.print("Enter name: ");
                 String name = input.askStr("Enter your ID");
                 Item item = new Item(name);
@@ -37,7 +36,7 @@ public class StartUI {
                 }
             } else if (select == 3) {
                 System.out.println("=== Delete item ===");
-                int id = Integer.parseInt(input.askStr("Enter your ID"));
+                int id = input.askInt("Enter your ID");
                 if (tracker.delete(id)) {
                     System.out.println("Заявка удалена успешно.");
                 } else {
@@ -45,8 +44,7 @@ public class StartUI {
                 }
             } else if (select == 4) {
                 System.out.println("=== Find item by id ===");
-                System.out.print("Enter id: ");
-                int id = Integer.parseInt(input.askStr("Enter your ID"));
+                int id = input.askInt("Enter your ID");
                 Item item = tracker.findById(id);
                 if (item != null) {
                     System.out.println(item);
