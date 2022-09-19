621f54c2931145e2d79f7cde74e10d004a56b378
sanya <batalovalexande@gmail.com>
2022-09-19 21:56:32 +0500

    4. Принципы раннего возврата и охранных выражений [#504875 #344646]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -3,14 +3,19 @@ package ru.job4j.early;
 public class PasswordValidator {
 
     public static String validate(String password) {
-
         String pass = "Qw254,W32";
         for (int index = 0; index < pass.length(); index++) {
             if (password.equals(password.toUpperCase())) {
                 throw new IllegalArgumentException( "The password must contain lowercase character.");
             }
         }
-        return password;
+        if (pass.length() < 8) {
+            throw new IllegalArgumentException("less than 8");
+        }
+        if (pass.length() < 32) {
+            throw new IllegalArgumentException("less than 32");
+        }
+        return pass;
     }
 }
 
